170b116b080d0e9b2401ad0fcb687529050e70a7
Ángela Donoso <angeladonoso12@gmail.com>
2025-05-25 16:31:57 +0200

    Creación de getById

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -2,11 +2,29 @@ const getAll = (req, res) => {
   res.send("Recupera todos los autores");
 };
 
+const getById = (req, res) => {
+  const { authorId } = req.params;
+
+  // Compruebo si authorId es un numero
+  if (isNaN(authorId)) {
+    res.status(400).send("El valor debe ser un número");
+  }
+
+  res.send("Recupero contacto por ID");
+};
+
 const create = (req, res) => {
-  res.send("Se crea un nuevo contacto");
+  const { nombre, email, telefono } = req.body;
+  res.send("Se crea un nuevo autor");
+};
+
+const edit = (req, res) => {
+  res.send("Se actualiza un autor");
 };
 
 module.exports = {
   getAll,
+  getById,
   create,
+  edit,
 };
--- src/routes/api/authors.routes.js
+++ src/routes/api/authors.routes.js
@@ -1,9 +1,16 @@
-const { getAll, create } = require("../../controllers/authors.controller");
+const {
+  getAll,
+  create,
+  edit,
+  getById,
+} = require("../../controllers/authors.controller");
 
 // peticiones de /api/authors
 const router = require("express").Router();
 
-router.get("", getAll);
-router.post("", create);
+router.get("/", getAll);
+router.get("/:authorId", getById);
+router.post("/", create);
+router.put("/", edit);
 
 module.exports = router;
